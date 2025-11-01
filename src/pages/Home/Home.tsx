1c5e600a744c6e361b22bcd0bf4773a75ad205d2
Eroma-EA <ergazi.avdanov.ea@gmail.com>
2025-11-01 16:26:54 +0500

    Initial commit

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section className="container fade-in">
+      <h1>Добро пожаловать!</h1>
+      <p>Приглашение на событие</p>
+    </section>
+  );
+};
+
+export default Home;
