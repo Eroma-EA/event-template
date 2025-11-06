92a8d3be1be5b8983cee0f379eb03080074b0235
Eroma-EA <ergazi.avdanov.ea@gmail.com>
2025-11-06 20:24:16 +0500

    update kz

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,14 @@
-import './Header.scss'
+import "./Header.scss";
 
 const Header = () => {
   return (
     <header className="header" aria-label="Сайт тақырыбы">
       <div className="header__inner">
         <h1 className="header__title">ТОЙ КҮНІ</h1>
-        <h2 className="header__names">АРМАН & ДИАНА</h2>
+        <h2 className="header__names">Айдос & Айгерім</h2>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
